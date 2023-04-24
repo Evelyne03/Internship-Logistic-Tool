1c5190163d1ed6cfd44016b74f47ccd01b3d2dc1
unknown <ionut.nechita01@e-uvt.ro>
2023-04-24 21:25:07 +0300

    Add spring boot configuration

---
+++ BackEnd/internshiptoolapp/src/main/java/com/internshiptoolapp/internshiptoolapp/InternshiptoolappApplication.java
@@ -0,0 +1,13 @@
+package com.internshiptoolapp.internshiptoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshiptoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshiptoolappApplication.class, args);
+	}
+
+}
---
+++ BackEnd/internshiptoolapp/src/test/java/com/internshiptoolapp/internshiptoolapp/InternshiptoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internshiptoolapp.internshiptoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshiptoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
