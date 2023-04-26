ffb50a29da1e97159a1567bb067f8f0834e1621c
DariusHoalba <hoalbadarius@gmail.com>
2023-04-26 21:57:58 +0300

    Added Maven dependencies

--- BackEnd/internshiptoolapp/src/main/java/com/internshiptoolapp/entity/User.java
+++ BackEnd/internshiptoolapp/src/main/java/com/internshiptoolapp/entity/User.java
@@ -1,10 +1,18 @@
 package com.internshiptoolapp.entity;
 
-import javax.persistence.Entity;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Entity
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    private String username;
+    private String password;
+    
+    // getters and setters
 }
