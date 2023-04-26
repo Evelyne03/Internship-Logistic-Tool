74e90a89d9e5ebaf73bae6940e17560c32b3633e
unknown <ionut.nechita01@e-uvt.ro>
2023-04-26 21:14:46 +0300

    Remove unnecessary folder

--- BackEnd/internshiptoolapp/src/main/java/com/internshiptoolapp/InternshiptoolappApplication.java
+++ BackEnd/internshiptoolapp/src/main/java/com/internshiptoolapp/InternshiptoolappApplication.java
@@ -1,4 +1,4 @@
-package com.internshiptoolapp.internshiptoolapp;
+package com.internshiptoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
