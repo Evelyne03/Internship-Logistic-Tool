755270fc3a78c9c00b4afa605c1c8c408e0eaaff
DariusHoalba <hoalbadarius@gmail.com>
2023-05-31 18:20:37 +0300

    Modified User Cards Info

--- FrontEnd/proiect-colectiv-angular/src/app/user-card/user-card.component.ts
+++ FrontEnd/proiect-colectiv-angular/src/app/user-card/user-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Team } from '../team';
+import { TeamService } from '../team.service';
 import {User} from '../user'
 import { UserService } from '../user.service';
 
@@ -11,11 +13,16 @@ export class UserCardComponent {
   @Input() user!: User;
   @Output() userDeleted = new EventEmitter<any>();
 
-  constructor(private UserService: UserService) { }
+  team!:Team;
+
+  constructor(private UserService: UserService, private TeamService:TeamService) { }
 
   ngOnInit(): void {
     if (this.user && typeof this.user === 'object') {
       this.user.image = this.user.image || 'https://www.w3schools.com/howto/img_avatar.png';
+      this.TeamService.getTeam(this.user.teamId).subscribe((team: Team) => {
+        this.team = team;
+      });
     }
   }
 
